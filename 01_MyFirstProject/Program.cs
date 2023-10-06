621f82d93faece57000ccfc23e1bc96adaac2f60
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-06 18:28:37 +0100

    while loop added to continue asking user for the correct name

--- 01_MyFirstProject/Program.cs
+++ 01_MyFirstProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,20 +14,24 @@ namespace _01_MyFirstProject
             // basic hello world type output to get things running
             Console.WriteLine("Welcome to my 50th C# project although this is the first on a new computer.");
 
-            // taking in name and greeting the person by name
-            Console.WriteLine("Please enter your name");
-            String personsName = Console.ReadLine();
+            // adding a while loop to restart the program if the name is not correct
+            while (true)
+            {
+                // taking in name and greeting the person by name
+                Console.WriteLine("Please enter your name");
+                String personsName = Console.ReadLine();
 
-            // validating persons name
-            if (personsName.Equals("Jordan", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine($"Welcome {personsName}"); // reading the name back to the person using newer formatting
-                Console.ReadLine(); // using this line to keep the cmd prmpt open
-            }
-            else
-            {
-                Console.WriteLine("Incorrect name, pls try again.");
-                Console.ReadLine();
+                // validating persons name
+                if (personsName.Equals("Jordan", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Welcome {personsName}"); // reading the name back to the person using newer formatting
+                    //Console.ReadLine(); // using this line to keep the cmd prmpt open
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect name, pls try again.");
+                }
             }
 
         }

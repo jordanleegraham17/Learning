4e0ceaa29c3eda4e7a192fe68b0fa06b929b68f4
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-09 16:01:11 +0100

    main method amendments

--- 01_If_Else_Statements/02_LoginIfElse.cs
+++ 01_If_Else_Statements/02_LoginIfElse.cs
@@ -10,9 +10,9 @@ namespace _01_If_Else_Statements
     /* ABOUT: this is a second mini project for practicing IF ELSE statements.
      * the goal for this class is to create a very basic version of a login system.
      */
-    internal class _02_LoginIfElse //learning about internal keyword for encapsulation - different to Java
+    class LoginSystem //learning about internal keyword for encapsulation - different to Java
     {
-        static void main()
+        public static void main(String[] args)
         {
             String userName = "Jordan";
             String passWord = "123456";
@@ -22,12 +22,40 @@ namespace _01_If_Else_Statements
             Console.WriteLine($"{welcomeMessage}");
 
             // objective 2: Prmpt the user for their userName
+            Console.WriteLine("Please enter your assigned Username:");
+            String userNameAttempt = Console.ReadLine();
 
             // objective 3: Accept user input and verify if it is the correct userName
-
-            // objective 4: If Else statement, if the userName is correct prompt the user for their passWord; else ask again
-
-            // objective 5: verify the user passWord, if correct welcome display "Logged In"
+            if (userNameAttempt != null)
+            {
+                // objective 4: If Else statement, if the userName is correct prompt the user for their passWord; else ask again
+                if (userNameAttempt == userName)
+                {
+                    Console.Write($"Welcome {userName}, please enter your password:");
+                    String passWordAttempt = Console.ReadLine();
+                    
+                    if(passWordAttempt != null)
+                    {
+                        // objective 5: verify the user passWord, if correct welcome display "Logged In"
+                        if (passWordAttempt == passWord)
+                        {
+                            Console.WriteLine("LOGGED IN WOO");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect Password");
+                        }
+                    }// end if password = null
+                } // end if username = username
+                else
+                {
+                    Console.WriteLine("Incorrect username, please try again");
+                }
+            }// end if username = null
+            else 
+            {
+                Console.WriteLine("Please enter a username.");
+            }
 
         }// end main
     }// end class

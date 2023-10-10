ce90a86baa4319da54d1a134fecdf173a310629e
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-10 18:03:59 +0100

    Created C_Ailerons file - this file requires work but used to quickly test if else statements after some time working on reverting git changes etc.

---
+++ 01_If_Else_Statements/C_Ailerons.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_If_Else_Statements
+{
+
+    /*
+     * This is quick if else statement practice to make sure it is staying with me over a few days.
+     * Would need to add user input validation to make this a valid mini project.
+     * I will work on validation going forward. Today learned a lot about revert git changes and changing startup project files.
+     */
+
+    class C_Ailerons
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please pick a direction to bank, type LEFT or RIGHT");
+            string angleToBank = Console.ReadLine();
+
+            while (true)
+            {
+                if (angleToBank == "left")
+                {
+                    Console.WriteLine("Left turn, Left aileron moves up, left wing moves down due to decrease in angle of attack. Right aileron angles down to increase angle of attack and raise the right wing.");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Right turn, Right aileron moves up, right wing moves down due to decrease in angle of attack. Left aileron angles down to increase angle of attack and raise the left wing.");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }//end main
+    }//end class
+}//end namespace

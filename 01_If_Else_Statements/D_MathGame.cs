fcb5db5c445140e93a0ba37017bc69cda643d6b0
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-16 14:45:09 +0100

    New: MathGame.cs class created and ready for code

---
+++ 01_If_Else_Statements/D_MathGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_If_Else_Statements
+{
+
+    /*
+     *  the goal of this class will be to practice if else statements
+     *  by creating a sample calculator which will add, multiply or subtract two values
+     *  based on the user input of 'add' , 'multiply' and 'subtract'.
+     *  
+     *  *I am aware of and have implemented switch statements in the past however,
+     *  I would like to get better at if else statments for the time being and focus on validating some inputs.
+     *  
+     *  Also for this class I would like to start implementing methods
+     */
+
+    class D_MathGame
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("MathGame.cs is set as the startup class");
+            Console.ReadLine(); // keeping the command prompt open
+        } //end main method
+    } //end class
+} //end namespace

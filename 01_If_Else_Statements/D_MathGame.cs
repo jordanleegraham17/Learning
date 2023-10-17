c79a5f7820cb144cf3d67d2e9c3a952c500c34c2
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-17 19:32:56 +0100

    Update: MathGame.cs methods for operations and user input validation for the operation choice.

--- 01_If_Else_Statements/D_MathGame.cs
+++ 01_If_Else_Statements/D_MathGame.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,8 +25,9 @@ namespace _01_If_Else_Statements
             Console.WriteLine("MathGame.cs is set as the startup class");
 
             // _______ code below here ____________
-            int valueOne, valueTwo, result; // create variables for the two values and the result
+            int valueOne, valueTwo; // create variables for the two values and the result
             bool isValidInput = false; //using this to validate user input
+            bool isValidOperation = false; // using this to validate user operation choice (for this practice have purposely avoided the switch statement)
 
             // while loop to continue asking user for an input
             while (!isValidInput)
@@ -46,17 +49,42 @@ namespace _01_If_Else_Statements
                         {
                             Console.WriteLine($"Your second value is: {valueTwo} ");
 
-                            // calculate and readback the result
-                            result = valueOne + valueTwo;
-                            Console.WriteLine($"{valueOne} + {valueTwo} = {result}");
-                            Console.ReadLine();
+                            //prompt user for a choice of operation
+                            while (!isValidOperation)
+                            {
+                                //prompt user for a choice of operation (add, subtract, divide, multiply)
+                                Console.WriteLine("Please enter the operation you would like to apply;");
+                                Console.WriteLine("Addition, Subtraction, Division, Multiplication:");
+                                // read the operation choice to decide on next action
+                                string operationChoice = Console.ReadLine();
+
+                                if (operationChoice.Equals("Addition", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    AddValues(valueOne,valueTwo);
+                                }
+                                else if (operationChoice.Equals("Subtraction", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    SubtractValues(valueOne,valueTwo);
+                                }
+                                else if (operationChoice.Equals("Division", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    DivideValues(valueOne, valueTwo);
+                                }
+                                else if (operationChoice.Equals("Multiplication", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    MultiplyValues(valueOne, valueTwo);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter a valid operation name.");
+                                }
+                            }//end while !validOperation
                         } //end if
                         else
                         {
                             Console.WriteLine("Incorrect input, please enter a valid integer");
                         } //end else
                     }// end while loop
-
                 }// end if
                 else
                 {
@@ -64,5 +92,37 @@ namespace _01_If_Else_Statements
                 }// end else
             }// end while          
         } //end main method
+
+        // addition method for adding both values together
+        static void AddValues(int valueOne, int ValueTwo)
+        {
+            int additionResult = valueOne + ValueTwo;
+            Console.WriteLine($"{valueOne} + {ValueTwo} = {additionResult}");
+            Console.ReadKey();
+        }
+
+        // subtraction method for subtracting values from one another
+        static void SubtractValues(int valueOne, int ValueTwo)
+        {
+            int subtractResult = valueOne - ValueTwo;
+            Console.WriteLine($"{valueOne} - {ValueTwo} = {subtractResult}");
+            Console.ReadKey();
+        }
+
+        // division method for dividing the values by one another, only the first input divided by second input
+        static void DivideValues(int valueOne, int valueTwo)
+        {
+            int divisionResult = valueOne / valueTwo;
+            Console.WriteLine($"{valueOne}/{valueTwo} = {divisionResult}");
+        }
+
+        // multiplication method for multiplying the two values entered by the user
+        static void MultiplyValues(int valueOne, int valueTwo)
+        {
+            int multiplicationResult = valueOne * valueTwo;
+            Console.WriteLine($"{valueOne} * {valueTwo} = {multiplicationResult}");
+            Console.ReadKey();
+        }
+
     } //end class
 } //end namespace

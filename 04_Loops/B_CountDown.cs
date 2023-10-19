12c859448c8876ec8a6e7c647c618e503ac9065b
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-19 17:19:26 +0100

    Updated: WhileLoops practice, removed redundant class and added new one to work on challenge of counting backward from user defined value.

---
+++ 04_Loops/B_CountDown.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class B_CountDown
+    {
+        /* This is a quick while loop practice - in this program I am trying to count backwards to the number 1 from a user defined number
+         * if the user enters 50; we will count backwards from 50 until we reach 1
+         */
+
+        static void Main(String[] args)
+        {
+            int currentValue = 20; // using a hardcoded entry number to start with - will make this user defined later
+            
+            while (currentValue >= 1)
+            {
+                currentValue--;
+                Console.WriteLine(currentValue.ToString());
+            }
+            Console.ReadKey();
+        }// end main
+    }//end class
+}//end namespace
---
+++ 04_Loops/B_WhileLoopsAlphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class B_WhileLoopsAlphabet
+    {
+        static void Main(String[] args)
+        {
+
+
+        }//end main method
+    }//end class
+}//end namespace

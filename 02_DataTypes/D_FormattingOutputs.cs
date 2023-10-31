8285e6b91f1535455f4350d74fd2268700acd749
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-10-31 12:04:31 +0000

    New: added outputs formatting class and string functions class

---
+++ 02_DataTypes/D_FormattingOutputs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * This class demonstrates basic output formatting, will be using this to show formats of currencies etc.
+*/
+
+namespace _01b_DataTypes
+{
+    class D_FormattingOutputs
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Currency: {0:c}", 25.645); //formatting a currency value (this shows dollar symbol)
+            Console.WriteLine("Pad w/ Zeros: {0:d4}", 28);
+            Console.WriteLine("3 Decimals: {0:f3}", 23.12331);
+            Console.WriteLine("Commas: {0:n4}", 2100);
+
+        }//end main
+    }//end class
+}//end namespace
---
+++ 02_DataTypes/E_StringFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01b_DataTypes
+{
+    class E_StringFunctions
+    {
+        static void Main(String[] args)
+        {
+            //find the length of a string
+            string randomString = "sophie is my friend";
+            Console.WriteLine(randomString);
+            Console.WriteLine(randomString.Length);
+
+            //check if a string contains specific values
+            Console.WriteLine("Does string contain 'sophie'? : {0}", randomString.Contains("sophie"));
+
+            //find the index (where in the string) is the value 'sophie'
+            Console.WriteLine("Index of the word 'sophie' : {0}", randomString.IndexOf("sophie"));
+
+        }//end main
+    }//end class
+}//end namespace

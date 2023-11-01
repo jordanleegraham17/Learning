9ca95143663185588fe2bfef060d317d89133fa8
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-11-01 16:17:21 +0000

    CURRENT: sum of integers, practicing loops. need to calculate the sum of all values  next.

---
+++ 04_Loops/D_SumOfIntegers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Using this class to practice the coding challenge 'sum of integers'. 
+ * The goal of this program will be to use a for loop or for each loop to calculate the sum of all integers
+ * from  1 - N , with N being a user defined value.
+ */
+
+namespace _04_Loops
+{
+    class D_SumOfIntegers
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please enter a value you would like to stop calculating at: ");
+            
+            // retrieving the user defined value
+            int userValueN = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nYou have entered: {userValueN}, which is of type: {userValueN.GetType()}");
+
+            // for loop to print each value from 1-N
+            for (int i = 1; i <= userValueN; i++)
+            {
+               // printing out each value between 1-N 
+               Console.WriteLine(i);
+            }//end for loop
+
+            Console.ReadKey();
+        }//end main
+    }//end class
+}//end namespace

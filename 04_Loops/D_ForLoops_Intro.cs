5f45caa309204c5b557b9c82634956d30be92738
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-11-01 15:00:37 +0000

    NEW: added class as an introduction to for loops. created basic output of each item in an array using a for loop.

---
+++ 04_Loops/D_ForLoops_Intro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class D_ForLoops_Intro
+    {
+        static void Main(String[] args)
+        {
+            //declaring an array (of type string)
+            string[] airlinesArray = { "Aer Lingus", "Ryanair", "Whizz Air" };
+
+            // practicing for loops
+            for (int i = 0; i < airlinesArray.Length; i++)
+            {
+                Console.WriteLine(airlinesArray[i]); // should print out each item of the for loop
+            }
+
+            Console.ReadKey();
+        }//end main
+    }//end class
+}//end namespace

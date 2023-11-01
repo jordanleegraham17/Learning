330e14b61fa4a91a150e0dffaf9a06e7539fe8ee
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-11-01 15:27:37 +0000

    ADDED: first basic function and for each used

--- 06_BasicFunctions/A_FirstFunctions.cs
+++ 06_BasicFunctions/A_FirstFunctions.cs
@@ -10,12 +10,23 @@ namespace _06_BasicFunctions
     {
         static void Main(string[] args)
         {
+            // defining an integer array to pass to function
+            int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            PrintArray(numbersArray, "ForEach");
+
+            Console.ReadKey();
         }//end main
 
         // _____ FUNCTIONS _____
 
-
+        static void PrintArray(int[] integerArray, string outputMessage)
+        {
+            foreach (int i in integerArray)
+            {
+                Console.WriteLine($"{outputMessage}, {i}");
+            }//end foreach
+        }//end printArray method
 
         // _____ END FUNCTIONS _____
 

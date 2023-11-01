85c33b88c24f7971ec7e5c28ffe17b77a384c4ef
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-11-01 15:13:24 +0000

    NEW: added a new project 06_Functions, for practicing basic functions outside of the main method

---
+++ 06_BasicFunctions/A_FirstFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BasicFunctions
+{
+    class A_FirstFunctions
+    {
+        static void Main(string[] args)
+        {
+
+        }//end main
+
+        // _____ FUNCTIONS _____
+
+
+
+        // _____ END FUNCTIONS _____
+
+    }//end class
+}//end namespace

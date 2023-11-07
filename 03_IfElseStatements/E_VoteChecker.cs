ef54c196aee07e583cb23a3a07248f8c9369aa94
jordanleegraham17 <jordanleegraham17@outlook.com>
2023-11-07 15:44:20 +0000

    UPDATE: E_VoteChecker, user input validation. Next step is to include while loop to continuously ask user for input when entering wrong input.

--- 03_IfElseStatements/E_VoteChecker.cs
+++ 03_IfElseStatements/E_VoteChecker.cs
@@ -16,11 +16,23 @@ namespace _01_If_Else_Statements
     {
         static void Main(String[] args)
         {
+            //variables
             string programDescription = "The purpose of this class is to do an age voting checker.\n" +
                 "A user will enter their age and the program will let them know if they can or cannot vote.\n" +
                 "A main goal for this class is to also incorporate user input validation, ensuring the user enters a valid integer\n\n";
+            int usersAge;
 
             Console.WriteLine(programDescription);
+            Console.WriteLine("Please enter your age: ");
+
+            if (int.TryParse(Console.ReadLine(), out usersAge))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer!");
+            }
 
 
             Console.ReadKey();//keeping the console alive

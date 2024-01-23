4efe18b9b19b593cdf7b0307bc48b9c1ae124fd1
jordanleegraham17 <jordanleegraham17@outlook.com>
2024-01-23 12:38:50 +0000

    Added FizzBuzz class to the functions folder. Fizzbuzz class setup finished ready for logic.

---
+++ 06_BasicFunctions/C_FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ABOUT
+ * FizzBuzz is a numerical game
+ * The game works by counting as normal
+ * The rules are:
+ * Whenever your number is divisible by 3 you instead say FIZZ! instead of your number.
+ * Whenever your number is divisible by 5 you instead say BUZZ! instead of your number.
+ * If your number is divisible by both 3 AND 5, you say FIZZBUZZ!! instead of your number.
+ * If your number doesn't meet any of the rules above, you simply say the number :).
+ */
+
+namespace _06_BasicFunctions
+{
+    class C_FizzBuzz
+    {
+
+        public static void Main(String[] args)
+        {
+
+            // define a number to start with
+            int startingValue = 0;
+
+            // begin counting from that number upwards
+            while (startingValue >= 0)
+            {
+                startingValue++;
+                Console.WriteLine(startingValue);
+            }//end while loop
+
+        }//end main
+    }//end class
+}//end namespace

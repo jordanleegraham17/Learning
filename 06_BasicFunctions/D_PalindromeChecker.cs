1c81479eec80439c3d1541d57fe8dead6911fafb
jordanleegraham17 <jordanleegraham17@outlook.com>
2024-01-26 15:43:38 +0000

    Created Palindrome Checker class and setup as Start Up class

---
+++ 06_BasicFunctions/D_PalindromeChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BasicFunctions
+{
+    class D_PalindromeChecker
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Palindrome Checker");
+            Console.ReadKey();
+        }//end main
+    }//end class
+}//end namespace

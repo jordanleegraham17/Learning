6f7113f988e78f77a6455f0f85a4fc05d4669961
jordanleegraham17 <jordanleegraham17@outlook.com>
2024-02-07 15:46:15 +0000

    Created the 'Objects' learning folder for re-learning objects , this time in C#. Set the introduction class as the entry point.

---
+++ 07_Objects/01_Introduction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Objects
+{
+    class _01_Introduction
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Introduction to C# objects");
+        }//end main
+    }//end class
+}//end namespace
